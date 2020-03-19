dac1bf579f02362d8ed798872af9509e878fbc66
Tadjaur <tadjauraurelien@gmail.com>
2020-03-19 13:19:53 +0000

    Delete socket.io.request.js

--- socket/socket.io.request.js
+++
@@ -1,9 +0,0 @@
-let aurelien_request = (client)=>{
-    client.on('aurelien_request', function (data) {
-        client.emit('aurelien_response', data + " 123")
-    })
-};
-
-module.exports = {
-    aurelien_request : aurelien_request,
-};
